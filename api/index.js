b13e242d77079a0de7e9cc114502577496dc7361
Getulio Gutemberg <getulio.dev@gmail.com>
2024-02-10 21:28:15 -0300

    atualiza app.js

--- api/index.js
+++ api/index.js
@@ -1,137 +1,15 @@
-const WebSocket = require('ws');
-const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000; // Porta onde o servidor irá escutar
+const cors = require('cors');
 
-// Conectar ao banco de dados MongoDB
-mongoose.connect('mongodb://localhost:27017/chatDB');
-const db = mongoose.connection;
+app.use(cors());
 
-// Definir esquemas e modelos para usuários, mensagens e logs
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  photo: String,
-  // Outros campos...
+app.get('/', (req, res) => {
+    res.send('Bem-vindo ao meu servidor backend em Node.js!');
 });
 
-const messageSchema = new mongoose.Schema({
-  sender: String,
-  recipient: String,
-  content: String,
-  // Outros campos...
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
-
-const logSchema = new mongoose.Schema({
-  message: String,
-  // Outros campos...
-});
-
-const User = mongoose.model('User', userSchema);
-const Message = mongoose.model('Message', messageSchema);
-const Log = mongoose.model('Log', logSchema);
-
-const wss = new WebSocket.Server({ port: 8080 });
-const clients = new Map();
-
-wss.on('connection', function connection(ws) {
-    
-    const clientId = generateClientId();
-    clients.set(clientId, ws);
-    
-    console.log('Nova conexão estabelecida.',clientId);
-
-  ws.on('message', function incoming(message) {
-    console.log('Mensagem recebida do cliente:', message);
-    handleMessage(clientId, message);
-  });
-
-  ws.on('close', function() {
-    console.log('Conexão fechada para o cliente:', clientId);
-    clients.delete(clientId);
-    notifyAllClientsExceptOne(clientId, 'Cliente desconectado: ' + clientId);
-  });
-});
-
-
-function handleMessage(clientId, message) {
-  const data = JSON.parse(message); // Parse the message = {"action":"create","type":"message","sender":"pessoaA","recipient":"pessoaB","content":"Mensagem aqui!"}
-    // console.log(clientId,data.action);
-    
-  switch (data.action) {
-    case 'create':
-      if (data.type === 'message') {
-        const newMessage = new Message({
-          sender: data.sender,
-          recipient: data.recipient,
-          content: data.content,
-        });
-        newMessage.save()
-          .then(() => {
-            notifyAllClients('Nova mensagem criada.' + newMessage.content);
-          })
-          .catch(error => {
-            console.error('Erro ao salvar mensagem:', error);
-          });
-      }
-      if (data.type === 'user') {
-        const newUser = new User({
-          name: data.username,
-          email: data.email,
-          password: data.password,
-          photo: data.photo
-        });
-        newUser.save()
-          .then(() => {
-            notifyAllClients('Novo usuário criado: ' + newUser.name);
-          })
-          .catch(error => {
-            console.error('Erro ao salvar usuário:', error);
-          });
-      }
-      if (data.type === 'log') {
-        const newLog = new Log({
-          message: data.message
-        });
-        newLog.save()
-          .then(() => {
-            notifyAllClients('Novo log criado: ' + newLog.message);
-          })
-          .catch(error => {
-            console.error('Erro ao salvar log:', error);
-          });
-      }
-      // Outros casos de criação para usuários, logs, etc.
-      break;
-    // Outros casos de ação...
-    case 'update':
-      
-      // Outros casos de ação...
-      break;
-    case 'delete':
-      // Outros casos de ação...
-      break;
-    default:
-      console.log('Ação não reconhecida:', data.action);
-  }
-}
-
-function notifyAllClients(message) {
-  clients.forEach((client, clientId) => {
-    client.send(message);
-  });
-}
-
-function notifyAllClientsExceptOne(excludedClientId, message) {
-  clients.forEach((client, clientId) => {
-    if (clientId !== excludedClientId) {
-      client.send(message);
-    }
-  });
-}
-
-function generateClientId() {
-  return 'client_' + Math.random().toString(36).substr(2, 9);
-}
-
-
 
--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactComponente from './ReactComponente'; 
+import './App.css';
+import axios from 'axios';
+
 
 function App() {
- return (
-   <div className="App">
+  const [responseAPI , setResponseAPI] = useState('Servidor não conectado');
+
+  useEffect(() => {    
+    axios.get('http://localhost:5000')
+    .then((response) => {
+      setResponseAPI('Conectado ao servidor com sucesso');
+      setTimeout(() => setResponseAPI(response.data), 2000);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+    
+  },[])
+  
+  return <div className="App">
      Hello React
      <ReactComponente />
+     {responseAPI}
      
    </div>
- );
+ 
 }
 export default App;
